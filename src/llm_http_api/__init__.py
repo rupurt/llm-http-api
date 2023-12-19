3ffcf7822c324d76a22a85af05f7632ad8ab7938
Alex Kwiatkowski <alex+git@fremantle.io>
2023-12-18 21:13:42 -0800

    add reload cli option

--- src/llm_http_api/__init__.py
+++ src/llm_http_api/__init__.py
@@ -9,7 +9,24 @@ def register_commands(cli):
     @click.option("-h", "--host", type=str, default="0.0.0.0", show_default=True)
     @click.option("-p", "--port", type=int, default=8080, show_default=True)
     @click.option("-l", "--log-level", type=str, default="info", show_default=True)
-    def http_api(host: str, port: int, log_level: str):
+    @click.option("-r", "--reload", is_flag=True, default=False, show_default=True)
+    @click.option(
+        "-d", "--reload-dirs", type=list[str], default=["src"], show_default=True
+    )
+    def http_api(
+        host: str,
+        port: int,
+        log_level: str,
+        reload: bool,
+        reload_dirs: list[str],
+    ):
         "Run a FastAPI HTTP server with OpenAI compatibility"
-        click.echo("Hello world!")
-        run(ServerSettings(host=host, port=port, log_level=log_level))
+        run(
+            ServerSettings(
+                host=host,
+                port=port,
+                log_level=log_level,
+                reload=reload,
+                reload_dirs=reload_dirs,
+            )
+        )
